767d8705edc151e12f6cd3d34141e4557de11578
juanjo7a0 <106703233+juanjo7a0@users.noreply.github.com>
2023-01-10 16:10:21 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
+import { InterpolacionComponent } from './clase4/interpolacion/interpolacion.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiPrimerComponenteComponent,
+    InterpolacionComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
